e8723ce100eec93081f3ef432c37d9279871a08b
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-14 08:43:40 -0700

    start of the user interface and get the data from the ajax and showing the data.

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,7 @@
 var Github = require("./../js/github.js").githubModule;
+
+github = new Github();
+
+github.getUser("Pyrrus");
+
+github.getRepos("Pyrrus");
--- js/github.js
+++ js/github.js
@@ -4,17 +4,16 @@ function Github() {
 	
 }
 
-Github.prototype.getName = function(userName) {
-	var user = 'https://api.github.com/users/'+username + '?access_token=' + apiKey;
-
+Github.prototype.getUser = function(userName) {
+	var user = 'https://api.github.com/users/'+userName + '?access_token=' + apiKey;
 	$.get(user).then(function(response) { 
 		console.log(response);
 	});
     
 };
 
-Github.prototype.getRepos = function(username) {
-	var userRepos  = 'https://api.github.com/users/'+username+'/repos?access_token=' + apiKey;
+Github.prototype.getRepos = function(userName) {
+	var userRepos  = 'https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey;
 	$.get(userRepos).then(function(response) { 
 		console.log(response);
 	});
