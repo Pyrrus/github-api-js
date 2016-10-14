4222a8fca8803e5fd86a3840aa8c90cbd355b1d9
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-14 10:32:12 -0700

    Done with the getting the image and user information. next part is the get the public repos.

--- js/github-interface.js
+++ js/github-interface.js
@@ -10,25 +10,36 @@ $(document).ready(function() {
   	var userName = $('#userName').val();
   	$("#loader").show();
   	github.getUser(userName, function(data) {
+  		console.log(data);
 	  	if (data.message === "Not Found" || userName === '') {
 	  		found = false;
 	  		$('#content').html("<h1>Not found any user :(</h1>");
 	  	} else {
-	  		var name = data.name;
-	  		var email = data.email;
-	  		var followers = data.followers;
-	  		var login = data.login;
-	  		var location = data.location;
-	  		var public_repos = data.public_repos; 
-	  		var updated_at = data.updated_at;
-	  		var private_repos = data.total_private_repos;
+	  		
+	  		var email = "<h3>" + data.email + "</h3>";
+	  		var followers = "<h3>Followers: " + data.followers + "</h3>";
+	  		var following =  "<h3>Following: " + data.following + "</h3>";
+	  		var login = "<h2>Username: " + data.login + "</h2>";
+	  		var location = "<h3>Location: " + data.location + "</h3>";
+	  		var public_repos = "<h3>Public repos: " + data.public_repos + "</h3>"; 
+	  		var updated_at = "<h3>Last update: " + data.updated_at + "</h3>";
+	  		var image = "<img src='" + data.avatar_url + "' alt='" + data.name + "' class='img-responsive' />";
+	  		var madeAt = "<h3>Created at: " + data.created_at + "</h3>";
 
-	  		if (name === undefined) {
-	  		  name = login;
+	  		if (data.name === undefined) {
+	  		  var name = "<h1>Name: " + data.login + "</h1>";
+	  		} else {
+	  		  var name = "<h1>Name: " + data.name + "</h1>";
 	  		}
 
-	  		
+	  		$('#content').append("<div class='row'>");
+	  		  $('#content').append("<div class='col-md-6' class='box'>" + image + "</div>");
+	  		  $('#content').append("<div class='col-md-6' class='box'>" + name + login + email + location + followers + following + public_repos + updated_at + madeAt + "</div>");
+	  		$('#content').append("</div>"); // end the first row
 
+	  		$('#content').append("<div class='row'>");
+	  		  $('#content').append("<div class='col-md-12' class='box'>" + image + "</div>");
+	  		$('#content').append("</div>"); // end the row
 	  	}
 
 	  	//github.getRepos(userName);
