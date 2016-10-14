be1942c46375cfc8eec067609f1b490aba180f0e
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-14 10:00:27 -0700

    Update some html to id then class. Also, set value from the first get like name.

--- js/github-interface.js
+++ js/github-interface.js
@@ -5,13 +5,38 @@ $(document).ready(function() {
   var github = new Github();
 
   $('#find').click(function() {
+  	$('#content').html('');
+  	var found = true;
   	var userName = $('#userName').val();
-
+  	$("#loader").show();
   	github.getUser(userName, function(data) {
-	  	console.log(data);
-  	});
+	  	if (data.message === "Not Found" || userName === '') {
+	  		found = false;
+	  		$('#content').html("<h1>Not found any user :(</h1>");
+	  	} else {
+	  		var name = data.name;
+	  		var email = data.email;
+	  		var followers = data.followers;
+	  		var login = data.login;
+	  		var location = data.location;
+	  		var public_repos = data.public_repos; 
+	  		var updated_at = data.updated_at;
+	  		var private_repos = data.total_private_repos;
 
-	//github.getRepos(userName);
+	  		if (name === undefined) {
+	  		  name = login;
+	  		}
+
+	  		
+
+	  	}
+
+	  	//github.getRepos(userName);
+
+	  	$('#content').show();
+	  	$("#loader").hide();
+  	});
+	
 
   });
 
--- js/github.js
+++ js/github.js
@@ -4,10 +4,8 @@ function Github() {
 	
 }
 
-//test
-
 Github.prototype.getUser = function(userName, userDisplay) {
-	var user = 'https://api.github.com/users/'+userName + '?access_token=' + apiKey;
+	var user = 'https://api.github.com/users/' + userName + '?access_token=' + apiKey;
     $.ajax({
       url: user,
       complete: function(data) {
@@ -17,7 +15,7 @@ Github.prototype.getUser = function(userName, userDisplay) {
 };
 
 Github.prototype.getRepos = function(userName, reposDisplay) {
-	var userRepos  = 'https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey;
+	var userRepos  = 'https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey;
 	$.ajax({
       url: userRepos,
       complete: function(data) {
