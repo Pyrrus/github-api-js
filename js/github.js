7b6e0c287f05d9c6749b60fa3ac7e2940fa05854
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-14 08:18:51 -0700

    Add the ajax request for the github.

--- js/github.js
+++ js/github.js
@@ -5,12 +5,19 @@ function Github() {
 }
 
 Github.prototype.getName = function(userName) {
-	var user   = 'https://api.github.com/users/'+username + '?access_token=' + apiKey;
+	var user = 'https://api.github.com/users/'+username + '?access_token=' + apiKey;
+
+	$.get(user).then(function(response) { 
+		console.log(response);
+	});
     
 };
 
 Github.prototype.getRepos = function(username) {
 	var userRepos  = 'https://api.github.com/users/'+username+'/repos?access_token=' + apiKey;
+	$.get(userRepos).then(function(response) { 
+		console.log(response);
+	});
 }
 
 exports.githubModule = Github;
